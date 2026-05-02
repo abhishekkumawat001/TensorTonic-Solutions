98dfc97a90c840dec6fc18c0b99644858e18256c
Abhishek kumawat <kabhishek2@iisc.ac.in>
2026-05-02 21:04:20 +0530

    Add solution for rnn-cell

---
+++ rnn/rnn-cell/rnn-cell.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def rnn_cell(x_t: np.ndarray, h_prev: np.ndarray, 
+             W_xh: np.ndarray, W_hh: np.ndarray, b_h: np.ndarray) -> np.ndarray:
+    """
+    Single RNN cell forward pass.
+    """
+    # YOUR CODE HERE
+    x = np.dot(W_xh, x_t) + np.dot(W_hh, h_prev) + b_h
+    y = np.tanh(x)
+
+    return y 
