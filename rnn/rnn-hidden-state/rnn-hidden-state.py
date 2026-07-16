c7b2f128324a3f79dfbcd13dc030b70a4defa95e
Abhishek kumawat <abhikumawat001@gmail.com>
2026-07-16 20:36:53 +0530

    Solve rnn-hidden-state

--- rnn/rnn-hidden-state/rnn-hidden-state.py
+++ rnn/rnn-hidden-state/rnn-hidden-state.py
@@ -5,6 +5,5 @@ def init_hidden(batch_size: int, hidden_dim: int) -> np.ndarray:
     Initialize the hidden state for an RNN.
     """
     # YOUR CODE HERE
-    init_state  = np.zeros((batch_size, hidden_dim), dtype = float)
-    
-    return init_state
+    h_init = np.zeros((batch_size, hidden_dim))
+    return h_init
