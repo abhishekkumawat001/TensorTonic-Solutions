9f55561a0c16d0c6a2d926a08a7d4915649b1cd2
Abhishek kumawat <abhikumawat001@gmail.com>
2026-07-24 22:35:57 +0530

    Solve gradient-clipping

--- gradient-clipping/gradient-clipping.py
+++ gradient-clipping/gradient-clipping.py
@@ -8,14 +8,10 @@ def clip_gradients(g, max_norm):
     g = np.asarray(g)
     norm = np.linalg.norm(g, keepdims = False)
 
-    if norm == 0 :
-        return g 
-    elif norm <= max_norm:
-        return g 
-    elif max_norm == 0:
-        return g 
-    elif max_norm < 0:
-        return g 
+    if norm == 0 or norm <= max_norm:
+        return g.copy() 
+    elif max_norm <= 0:
+        return g.copy() 
     else:
         return g * (max_norm/norm)
         
